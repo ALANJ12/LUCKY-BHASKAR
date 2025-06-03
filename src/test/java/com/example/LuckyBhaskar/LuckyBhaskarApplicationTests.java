b7b7b5c5138b37bd7334fd439850b4d139ade4cc
jackson <jacksonj0007@gmail.com>
2025-06-03 14:38:12 +0530

    started

---
+++ src/test/java/com/example/LuckyBhaskar/LuckyBhaskarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LuckyBhaskar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyBhaskarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
